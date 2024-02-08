a1245b13dc913c71b81e5e2bcd2e0d5f0e3d0eb3
vis-110 <vishnuravi15898@gmail.com>
2024-02-08 20:37:25 +0530

    write a reponsive code

--- responsive-using-mui-grid/responsive-using-mui-grid/src/App.js
+++ responsive-using-mui-grid/responsive-using-mui-grid/src/App.js
@@ -1,38 +1,55 @@
+import TextField from '@mui/material/TextField';
 import './App.css';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Unstable_Grid2';
+import Container  from '@mui/material/Container';
+import Typography  from '@mui/material/Typography';
+import { Button } from '@mui/material';
 function App() {
   return (
-    <Box >
-    <h1>
-      React project in grid responsive
-    </h1>
-
-    <Grid container spacing={3}>
-        <Grid item xs={12} sx={{backgroundColor:'lightgray'}}>
-          Number 1
+    <Box sx={{ width: '100%', margin:'50px 0px' }}>
+      <Box sx={{textAlign:'center'}}>
+        <Typography variant='h5' sx={{margin:'50px 0px'}}>Material UI using responsive</Typography>
+      </Box>
+    <Container>
+      <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid  sm={12} md={6} lg={4} >
+          <Paper elevation={3}>
+            <TextField fullWidth variant='outlined' label='First Name'></TextField>
+          </Paper>
         </Grid>
-        <Grid item xs={6} sx={{backgroundColor:'lightgreen'}}>
-        Number 2        
+        <Grid  sm={12} md={6} lg={4} >
+          <Paper elevation={3}>
+            <TextField fullWidth variant='outlined' label='Last Name' ></TextField>
+          </Paper>
         </Grid>
-        <Grid item xs={6} sx={{backgroundColor:'yellow'}}>
-        Number 3       
-         </Grid>
-        <Grid item xs={3} sx={{backgroundColor:'lightblue'}}>
-        Number 4
+        <Grid sm={12} md={6} lg={4} >
+          <Paper elevation={3}>
+            <TextField fullWidth variant='outlined' label='Email'></TextField>
+          </Paper>
         </Grid>
-        <Grid item xs={3} sx={{backgroundColor:'yellow'}}>
-        Number 5
+        <Grid  sm={12} md={6} lg={4}>
+          <Paper elevation={3}>
+            <TextField fullWidth variant='outlined' label='Age'></TextField>
+          </Paper>
         </Grid>
-        <Grid item xs={3} sx={{backgroundColor:'lightgreen'}}>
-        Number 6
+        <Grid  sm={12} md={6} lg={4} >
+          <Paper elevation={3}>
+            <TextField fullWidth variant='outlined' label='Password'></TextField>
+          </Paper>
         </Grid>
-        <Grid item xs={3} sx={{backgroundColor:'lightgray'}}>
-        Number 7
+        <Grid sm={12} md={6} lg={4}>
+          <Paper elevation={3}>
+            <TextField fullWidth variant='outlined' label='Retype Password'></TextField>
+          </Paper>
+        </Grid>
+        
+        <Grid sx={{textAlign:'center'}} sm={12} md={12} lg={12}>
+            <Button sx={{margin:'20px 0px'}} variant='contained'>Submit</Button>
         </Grid>
       </Grid>
-
+      </Container>
     </Box>
   );
 }
